0e8168c60a598d22256fc31323e53de17e624812
the-code-papi <kigenbrandon@gmail.com>
2024-02-10 19:14:58 +0300

    created the base and Product page components and custom hooks

--- src/context/PorductsProvider.tsx
+++ src/context/PorductsProvider.tsx
@@ -7,7 +7,7 @@ export type ProductType = {
 };
 
 const initState: ProductType[] = [
-  /* {
+  {
     sku: "item0001",
     name: "Widget",
     price: 9.99,
@@ -21,7 +21,7 @@ const initState: ProductType[] = [
     sku: "item0003",
     name: "Deluxe Widget",
     price: 29.99,
-  }, */
+  },
 ];
 
 export type UseProductContextType = { products: ProductType[] };
@@ -34,24 +34,24 @@ type ChildrenType = {
   children?: ReactElement | ReactElement[];
 };
 
-export const ProdustsProvider = ({ children }: ChildrenType): ReactElement => {
+export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState);
 
-  useEffect(() => {
-    const fetchProducts = async (): Promise<ProductType[]> => {
-      const data = await fetch("http://localhost:3500/products")
-        .then((res) => {
-          return res.json();
-        })
-        .catch((err) => {
-          if (err instanceof Error) console.dir(err);
-        });
+  // useEffect(() => {
+  //   const fetchProducts = async (): Promise<ProductType[]> => {
+  //     const data = await fetch("http://localhost:3500/products")
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .catch((err) => {
+  //         if (err instanceof Error) console.dir(err);
+  //       });
 
-      return data;
-    };
+  //     return data;
+  //   };
 
-    fetchProducts().then((products) => setProducts(products));
-  }, []);
+  //   fetchProducts().then((products) => setProducts(products));
+  // }, []);
 
   return (
     <ProductsContext.Provider value={{ products }}>
